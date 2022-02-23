16ee3dd2dcda0ad30bef1d621ac641066d520c3c
pavlo.bokhan <pavlobokhan.jointoit@gmail.com>
2022-02-23 22:52:33 +0200

    fix: style item

--- src/components/CurrencieItem.vue
+++ src/components/CurrencieItem.vue
@@ -1,10 +1,10 @@
 <template>
   <div
-    class="allCurrencies-item"
+    class="currencieItem"
     @click.self="$router.push({ name: 'Currencie', params: { id: data.id } })"
   >
     {{ data.label }}
-    <div class="allCurrencies-item__fav" @click="changeFav(data.id)">
+    <div class="currencieItem__fav" @click="changeFav(data.id)">
       <Star :type="data.fav" />
     </div>
   </div>
@@ -27,4 +27,28 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+.currencieItem
+  width: 700px
+  height: 80px
+  margin-bottom: 10px
+  background: var(--background-color)
+  color: var(--text-color)
+  font-size: 20px
+  border: 2px solid #DDDDDD
+  padding: 15px
+  display: flex
+  align-items: center
+  justify-content: space-between
+
+  &:hover
+    border: 2px solid var(--text-color)
+
+  &__fav
+    height: 32px
+    width: 32px
+    cursor: pointer
+
+    svg path
+      stroke: var(--text-color)
+      fill: var(--text-color)
 </style>
